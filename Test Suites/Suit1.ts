27efc90511477706e4285e14b3d8e04aa819df75
HoangPhuc <maximumz160@gmail.com>
2020-10-06 17:06:43 +0700

    Lab 04 - 05

---
+++ Test Suites/Suit1.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Suit1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>b04cde63-6e6f-4075-b9c3-5d3684398bc2</testSuiteGuid>
+</TestSuiteEntity>
